365b241739826117d11f5810e3b82d2792f1c3b3
Shota HAMADA <maku3ts@gmail.com>
2018-07-12 02:25:31 +0900

    Do not use quotes for system header include declaration

--- Dx11Xaml/pch.h
+++ Dx11Xaml/pch.h
@@ -7,12 +7,13 @@
 
 #define NOMINMAX
 
-#include "winrt/Windows.Foundation.h"
-#include "winrt/Windows.ApplicationModel.Activation.h"
-#include "winrt/Windows.UI.Xaml.h"
-#include "winrt/Windows.UI.Xaml.Controls.h"
-#include "winrt/Windows.UI.Xaml.Controls.Primitives.h"
-#include "winrt/Windows.UI.Xaml.Data.h"
-#include "winrt/Windows.UI.Xaml.Interop.h"
-#include "winrt/Windows.UI.Xaml.Markup.h"
-#include "winrt/Windows.UI.Xaml.Navigation.h"
+#include <windows.ui.xaml.media.dxinterop.h>
+#include <winrt/Windows.ApplicationModel.Activation.h>
+#include <winrt/Windows.Foundation.h>
+#include <winrt/Windows.UI.Xaml.Controls.Primitives.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Data.h>
+#include <winrt/Windows.UI.Xaml.Interop.h>
+#include <winrt/Windows.UI.Xaml.Markup.h>
+#include <winrt/Windows.UI.Xaml.Navigation.h>
+#include <winrt/Windows.UI.Xaml.h>
