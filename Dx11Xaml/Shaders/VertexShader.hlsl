b683c389c7bc313a81493c7470659588e6460c1b
Shota HAMADA <maku3ts@gmail.com>
2018-07-15 16:18:20 +0900

    Fix vertex shader argument semantics typo

--- Dx11Xaml/Shaders/VertexShader.hlsl
+++ Dx11Xaml/Shaders/VertexShader.hlsl
@@ -1,3 +1,3 @@
-float4 main(float3 position : Position) : SV_Position {
-  return float4(position, 1.0);
+float4 main(float3 position : SV_Position) : SV_Position {
+  return float4(position, 1);
 }
